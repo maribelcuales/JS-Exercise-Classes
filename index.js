e31607ad456a8264f78d3d4d292bf39911004ee0
maribelcuales <maribelcuales@gmail.com>
2020-04-14 13:18:51 +0800

    Created class Car and passed tests for initializing Car arguments.

--- index.js
+++ index.js
@@ -98,7 +98,12 @@ console.log(sarah.toString());
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0; 
+  }
 }
 
 /*
