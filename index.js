640db7677aa67b5f6192c52398d19e10d176bea2
maribelcuales <maribelcuales@gmail.com>
2020-04-15 02:02:51 +0800

    Created child class Student and passed tests.

--- index.js
+++ index.js
@@ -237,7 +237,13 @@ console.log(mark.grade(student, 'python'));
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor(attributes){
+    super(attributes)
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className; 
+    this.favSubjects = attributes.favSubjects;
+  }
 
 }
 
